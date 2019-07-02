615c47eae5f8883d8e712e7df407f7158d202fe4
Brian Price <gblump@gmail.com>
2019-07-01 19:46:44 -0500

    Addiing additional information from winrm exceptions.

--- winrm/exceptions.py
+++ winrm/exceptions.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
+import logging
+import xmltodict
+
+LOGGER = logging.getLogger(__name__)
 
 class WinRMError(Exception):
     """"Generic WinRM error"""
@@ -19,11 +23,46 @@ class WinRMTransportError(Exception):
 
     @property
     def message(self):
-        return 'Bad HTTP response returned from server. Code {0}'.format(self.code)
+        return 'Bad HTTP response returned from server. Code {status_code}: {reason}'.format(
+            status_code=self.code, reason=self.winrm_fault_reason)
 
     @property
     def response_text(self):
         return self.args[2]
+
+    @property
+    def response_dict(self):
+        try:
+            return xmltodict.parse(self.response_text)
+        except (xmltodict.expat.ExpatError, TypeError):
+            return None
+
+    @property
+    def winrm_fault_code(self):
+        if isinstance(self.response_dict, dict):
+            try:
+                return self.response_dict['s:Envelope']['s:Body']['s:Fault']['s:Code']['s:Value']
+            except (KeyError, AttributeError, TypeError) as exc:
+                LOGGER.warning("Unable to find fault code: {exc}".format(exc=exc))
+        return None
+
+    @property
+    def winrm_fault_subcode(self):
+        if isinstance(self.response_dict, dict):
+            try:
+                return self.response_dict['s:Envelope']['s:Body']['s:Fault']['s:Code']['s:Subcode']['s:Value']
+            except (KeyError, AttributeError, TypeError) as exc:
+                LOGGER.warning("Unable to find fault sub code: {exc}".format(exc=exc))
+        return None
+
+    @property
+    def winrm_fault_reason(self):
+        if isinstance(self.response_dict, dict):
+            try:
+                return self.response_dict['s:Envelope']['s:Body']['s:Fault']['s:Reason']['s:Text']['#text']
+            except (KeyError, AttributeError, TypeError) as exc:
+                LOGGER.warning("Unable to find fault sub code: {exc}".format(exc=exc))
+        return None
 
     def __str__(self):
         return self.message
